b4bb458e24f38ecc37b9f064af4dfead0250eac4
Waldos-hub <regalia.guido@gmail.com>
2025-12-08 18:22:21 -0300

    Cree index y Gui bases

---
+++ Frontend/GUI.java
@@ -0,0 +1,15 @@
+//Importar paquetes
+import javax.swing.*;
+//import java.awt.*;
+
+public class GUI extends JFrame{
+    //Constructor
+    public GUI() {
+        //Atributos del frame
+        super("LDR");
+        setSize(1280,720);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
---
+++ Frontend/index.java
@@ -0,0 +1,6 @@
+public class index{
+    public static void main(String args[]) {
+        GUI gui= new GUI();
+        gui.setLocationRelativeTo(null); // Centrar en pantalla
+    }
+}
